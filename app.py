e37f8f494e8c676207e5b5d5509aed888d3162b1
regitahadi <150149645+regitahadi@users.noreply.github.com>
2025-06-29 10:06:05 +0700

    menambah preprocessing

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import os
+import re
+import string
 import gdown
 import streamlit as st
 import torch
+import pandas as pd
 from transformers import BertTokenizer, BertConfig, BertForSequenceClassification
 
-# Link Google Drive ke file model
+# ======== MODEL FILES =========
 MODEL_FILES = {
     "config.json": "https://drive.google.com/uc?id=1-gaeEZS51znvhsvSdnez9XB-xKvR2Dih",
     "best_model.pt": "https://drive.google.com/uc?id=1USuLqLopkGwJY6EtBTARdor6qvHcowW_",
@@ -12,8 +15,10 @@ MODEL_FILES = {
     "special_tokens_map.json": "https://drive.google.com/uc?id=1-lurkvcFx02DmjMqzIc9Z4LGRGRQ9AuS",
     "tokenizer_config.json": "https://drive.google.com/uc?id=1-tHk4S9UMk3xdosTpkgeCkxsP3JWB2xJ"
 }
-
 MODEL_FOLDER = "petugas_model"
+
+KAMUS_CSV_URL = "https://drive.google.com/uc?id=1fGWZu5qVYJa-pv078spaLE4urs5zDDPV"
+KAMUS_PATH = "kamus.csv"
 
 def download_model():
     if not os.path.exists(MODEL_FOLDER):
@@ -26,6 +31,11 @@ def download_model():
             with st.spinner(f"Mengunduh {filename}..."):
                 gdown.download(url, path, quiet=False)
 
+def download_kamus():
+    if not os.path.exists(KAMUS_PATH):
+        with st.spinner("Mengunduh kamus slang..."):
+            gdown.download(KAMUS_CSV_URL, KAMUS_PATH, quiet=False)
+
 @st.cache_resource(show_spinner=True)
 def load_tokenizer():
     return BertTokenizer.from_pretrained(MODEL_FOLDER)
@@ -37,13 +47,28 @@ def load_model():
     model.eval()
     return model
 
+@st.cache_resource
+def load_kamus():
+    df = pd.read_csv(KAMUS_PATH)
+    return dict(zip(df['slang'], df['formal']))
+
+def preprocess(text, kamus_slang):
+    text = text.lower()
+    text = re.sub(r'http\S+|www\S+|@\S+|#\S+', '', text)
+    text = re.sub(r'\d+', '', text)
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    text = ' '.join([kamus_slang.get(word, word) for word in text.split()])
+    return text.strip()
+
 def main():
     st.title("📌 Prediksi Aspek: Petugas Haji")
 
     download_model()
+    download_kamus()
 
     tokenizer = load_tokenizer()
     model = load_model()
+    kamus_slang = load_kamus()
 
     text = st.text_area("Masukkan teks:", height=150)
 
@@ -52,7 +77,9 @@ def main():
             st.warning("Masukkan teks dulu ya!")
             return
 
-        inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True, max_length=128)
+        cleaned_text = preprocess(text, kamus_slang)
+        inputs = tokenizer(cleaned_text, return_tensors="pt", padding=True, truncation=True, max_length=128)
+
         with torch.no_grad():
             outputs = model(**inputs)
         logits = outputs.logits
